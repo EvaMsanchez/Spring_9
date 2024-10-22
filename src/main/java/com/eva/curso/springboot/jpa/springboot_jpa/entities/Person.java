50a548a0053dda81fd891fc57e1703a72480f7d4
EvaMsanchez <eva_crm20@hotmail.com>
2024-10-22 11:38:41 +0200

    Creando la clase anotada con @Entity

---
+++ src/main/java/com/eva/curso/springboot/jpa/springboot_jpa/entities/Person.java
@@ -0,0 +1,67 @@
+package com.eva.curso.springboot.jpa.springboot_jpa.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="persons")
+public class Person 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // indicar cual es el tipo de dato
+    private Long id;
+
+    private String name;
+    private String lastname;
+
+    @Column(name="programming_language")
+    private String programmingLanguage;
+
+    // Obligatorio, ya que Hibernate utiliza el constructor vacío para poblar tabla
+    public Person() {}
+
+    public Person(Long id, String name, String lastname, String programmingLanguage) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.programmingLanguage = programmingLanguage;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    // Por si queremos imprimir el objeto persona
+    @Override
+    public String toString() {
+        return "[id=" + id + ", name=" + name + ", lastname=" + lastname + ", programmingLanguage="
+                + programmingLanguage + "]";
+    }
+    
+}
