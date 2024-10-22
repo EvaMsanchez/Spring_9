dec2993633d3767c74a0ef6914cfcf778dd7b7c8
EvaMsanchez <eva_crm20@hotmail.com>
2024-10-22 14:08:20 +0200

    Creando la tabla y agregando datos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Andrés', 'Guzmán', 'Java');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Pepe', 'Doe', 'Python');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('John', 'Dow', 'JavaScript');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('María', 'Roe', 'Java');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Josefa', 'Rae', 'Java');
